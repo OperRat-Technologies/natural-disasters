2b7019e769b404d3e2649f32e9c159d84599af19
tcklpl <negroniluan@gmail.com>
2024-10-20 20:10:16 -0300

    some kotlin changes
    Signed-off-by: tcklpl <negroniluan@gmail.com>

--- src/main/java/me/tcklpl/naturaldisaster/data/Vec3i.kt
+++ src/main/java/me/tcklpl/naturaldisaster/data/Vec3i.kt
@@ -1,36 +1,3 @@
-package me.tcklpl.naturaldisaster.data;
+package me.tcklpl.naturaldisaster.data
 
-public class Vec3i {
-
-    private int x, y, z;
-
-    public Vec3i(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getZ() {
-        return z;
-    }
-
-    public void setZ(int z) {
-        this.z = z;
-    }
-}
+data class Vec3i(val x: Int, val y: Int, val z: Int)
