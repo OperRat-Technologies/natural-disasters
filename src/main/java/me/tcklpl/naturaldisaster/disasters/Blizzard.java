f56267bbf47b4076c0d3d1952345709a4d347f31
tcklpl <negroniluan@gmail.com>
2024-10-20 19:50:48 -0300

    Blizzard revamp
    Signed-off-by: tcklpl <negroniluan@gmail.com>

--- src/main/java/me/tcklpl/naturaldisaster/disasters/Blizzard.java
+++ src/main/java/me/tcklpl/naturaldisaster/disasters/Blizzard.java
@@ -7,29 +7,27 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.type.Snow;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.util.BoundingBox;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Blizzard extends Disaster {
+
+    private HashMap<Integer, List<Block>> blocksToChangePerLevel = new HashMap<>();
 
     public Blizzard() {
         super("Blizzard", true, Material.SNOWBALL, BiomeUtils.PrecipitationRequirements.SHOULD_SNOW);
     }
 
     @Override
-    public void startDisaster() {
-
-        super.startDisaster();
-
-        Random r = random;
-
-        HashMap<Integer, List<Block>> blocksToChangePerLevel = new HashMap<>();
+    public void setupDisaster() {
+        super.setupDisaster();
+        blocksToChangePerLevel = new HashMap<>();
 
         for (int y = map.getHighestCoordsLocation().getBlockY(); y >= map.getLowestCoordsLocation().getBlockY(); y--) {
             List<Block> layerBlocks = new ArrayList<>();
@@ -45,27 +43,97 @@ public class Blizzard extends Disaster {
         }
 
         map.setPrecipitation(DisasterMap.MapPrecipitation.PRECIPITATE);
+    }
 
-        AtomicInteger timesRunned = new AtomicInteger(0);
-        AtomicInteger nextIteration = new AtomicInteger(10 + r.nextInt(6));
-        AtomicInteger currentY = new AtomicInteger(map.getMaxYLevel());
+    private int numPartialLayers(int currentY, int maxLayers, int minLayers) {
+        float diff = maxLayers - minLayers;
+        float percentage = ((float) currentY - (float) map.getMinY()) / ((float) map.getMapSize().getY());
+        return Math.round(minLayers + diff * percentage);
+    }
 
-        List<Material> blockPallete = new ArrayList<>();
-        blockPallete.add(Material.PACKED_ICE);
-        blockPallete.add(Material.ICE);
+    private final List<Double> weights = Arrays.asList(0.5, 0.3, 0.2, 0.1, 0.05, 0.01, 0.005, 0.001);
 
-        int taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> {
+    @Override
+    public void startDisaster() {
+        super.startDisaster();
 
+        // -------------------------------------------------------------------------------------------------------------
+        // Map freezing
+        // -------------------------------------------------------------------------------------------------------------
+        AtomicInteger currentY = new AtomicInteger(map.getMaxY());
+
+        List<Material> blockPallete = Arrays.asList(Material.BLUE_ICE, Material.PACKED_ICE, Material.ICE);
+        int maxPartialLayers = 8;
+        int minPartialLayers = 2;
+
+        int freezeTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> {
             if (currentY.get() >= map.getLowestCoordsLocation().getBlockY()) {
-                if ((timesRunned.incrementAndGet() % nextIteration.get()) == 0) {
+                map.bufferedReplaceBlocks(blocksToChangePerLevel.get(currentY.get()), blockPallete, 500, false);
 
-                    nextIteration.set(5 + r.nextInt(5));
-                    map.bufferedReplaceBlocks(blocksToChangePerLevel.get(currentY.getAndDecrement()), blockPallete, 500, false);
+                int layersBelowToPartiallyConvert = numPartialLayers(currentY.get(), maxPartialLayers, minPartialLayers);
+                var extraBlocksToReplace = new HashSet<Block>();
+                for (int i = 1; i <= layersBelowToPartiallyConvert; i++) {
+                    int curLowerY = currentY.get() - i;
+                    if (curLowerY < map.getMinY()) break;
+                    var layerBlocks = blocksToChangePerLevel.get(curLowerY);
+                    double weight = weights.get(i - 1);
+                    long blocksToPick = Math.round(layerBlocks.size() * weight);
 
+                    for (int j = 0; j < blocksToPick; j++) {
+                        var pickedBlock = random.nextInt(layerBlocks.size());
+                        extraBlocksToReplace.add(layerBlocks.get(pickedBlock));
+                    }
                 }
-            }
 
-            if (map.getPlayersInArena().size() > 0) {
+                map.bufferedReplaceBlocks(extraBlocksToReplace.stream().toList(), blockPallete, 500, false);
+                currentY.decrementAndGet();
+            }
+        }, startDelay, 60L);
+
+        // -------------------------------------------------------------------------------------------------------------
+        // Snow layers increasing
+        // -------------------------------------------------------------------------------------------------------------
+        var blocksAddSnowLayers = new AtomicInteger(50);
+
+        int snowTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> {
+            for (int i = 0; i < blocksAddSnowLayers.get(); i++) {
+                var randomLocation = map.getRandomBlockInMap();
+
+                // if it's a solid block, get the one on top
+                if (!randomLocation.getBlock().isPassable()) {
+                    randomLocation.setY(randomLocation.getY() + 1);
+                }
+
+                if (!checkIfBlockCanBecomeSnowLayer(randomLocation.getBlock())) continue;
+
+                // snow layer already? add another layer
+                if (randomLocation.getBlock().getType().equals(Material.SNOW)) {
+                    Snow snowBlock = (Snow) randomLocation.getBlock().getBlockData();
+                    int newLayers = snowBlock.getLayers() + 1;
+
+                    // already max layers? set as snow block
+                    if (newLayers >= snowBlock.getMaximumLayers()) {
+                        randomLocation.getBlock().setType(Material.SNOW_BLOCK);
+                    }
+                    // if not, just add another layer
+                    else {
+                        snowBlock.setLayers(newLayers);
+                        randomLocation.getBlock().setBlockData(snowBlock);
+                    }
+                }
+                // not a snow layer, set block as snow layer
+                else {
+                    randomLocation.getBlock().setType(Material.SNOW);
+                }
+
+            }
+        }, 50L, 10L);
+
+        // -------------------------------------------------------------------------------------------------------------
+        // Damage players
+        // -------------------------------------------------------------------------------------------------------------
+        int damageTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> {
+            if (!map.getPlayersInArena().isEmpty()) {
                 for (Player p : map.getPlayersInArena()) {
 
                     assert p != null;
@@ -94,10 +162,28 @@ public class Blizzard extends Disaster {
 
                 }
             }
-
         }, startDelay, 20L);
 
-        registerTasks(taskId);
+        registerTasks(freezeTask, snowTask, damageTask);
+    }
 
+    private boolean checkIfBlockCanBecomeSnowLayer(Block b) {
+        var blockBelow = b.getRelative(BlockFace.DOWN);
+
+        // blocks without collision
+        if (blockBelow.isPassable()) return false;
+
+        // liquids
+        if (blockBelow.isLiquid()) return false;
+
+        // collision with more than 1 bounding box
+        var voxelShape = blockBelow.getCollisionShape();
+        if (voxelShape.getBoundingBoxes().size() != 1) return false;
+
+        // non-cube bounding box
+        BoundingBox boundingBox = (BoundingBox) voxelShape.getBoundingBoxes().toArray()[0];
+        return boundingBox.getWidthX() == 1.0 &&
+                boundingBox.getHeight() == 1.0 &&
+                boundingBox.getWidthZ() == 1.0;
     }
 }
