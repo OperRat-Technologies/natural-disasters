b2809ed2d64517d239a12864de55ac0689703130
tcklpl <negroniluan@gmail.com>
2024-10-20 20:10:26 -0300

    revamped thunderstorm
    Signed-off-by: tcklpl <negroniluan@gmail.com>

--- src/main/java/me/tcklpl/naturaldisaster/disasters/Thunderstorm.java
+++ src/main/java/me/tcklpl/naturaldisaster/disasters/Thunderstorm.java
@@ -3,109 +3,95 @@ package me.tcklpl.naturaldisaster.disasters;
 import me.tcklpl.naturaldisaster.map.DisasterMap;
 import me.tcklpl.naturaldisaster.util.BiomeUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LightningStrike;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Thunderstorm extends Disaster {
 
     public Thunderstorm() {
-        super("Thunderstorm", false, Material.CREEPER_HEAD, BiomeUtils.PrecipitationRequirements.SHOULD_RAIN);
+        super("Thunderstorm", true, Material.CREEPER_HEAD, BiomeUtils.PrecipitationRequirements.SHOULD_RAIN);
+    }
+
+    private void spawnLightningStrikeAt(Location loc) {
+        LightningStrike ls = map.getWorld().spawn(loc, LightningStrike.class);
+        Block b = ls.getLocation().getBlock();
+
+        // break blocks around the lighting strike
+        Arrays.stream(BlockFace.values()).map(b::getRelative).forEach(rel -> rel.breakNaturally(new ItemStack(Material.AIR)));
     }
 
     @Override
     public void startDisaster() {
-        //map.getWorld().setMonsterSpawnLimit(100);
         super.startDisaster();
-
         map.setPrecipitation(DisasterMap.MapPrecipitation.PRECIPITATE);
 
         Random r = random;
 
-        HashMap<Location, Integer> creeperChargedSpawns = new HashMap<>();
-        AtomicInteger timesRunned = new AtomicInteger(0);
+        AtomicInteger randomStrikesTimesRan = new AtomicInteger(0);
+        AtomicInteger randomLightningStrikes = new AtomicInteger(1);
 
-        int taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> {
+        // Spawn random lightning strikes on the map
+        int randomLightningTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> {
 
-            if ((timesRunned.incrementAndGet() % 3) == 0)
-                for (Player p : map.getPlayersInArena()) {
+            for (var i = 0; i < randomLightningStrikes.get(); i++) {
+                Bukkit.getScheduler().scheduleSyncDelayedTask(main, () -> {
 
-                    assert p != null;
-                    Location l = p.getLocation();
+                    var loc = map.getRandomBlockInMap();
+                    spawnLightningStrikeAt(loc);
 
-                    if (p.getGameMode() == GameMode.ADVENTURE) {
-
-                        int chanceOfStrike = 15;
-                        int playersNearby = 0;
-
-                        // Lightning chance of spawning per factor
-                        if (l.getY() >= Math.floorDiv(map.getMapSize().getY(), 2))
-                            chanceOfStrike += 30;
-                        if (map.getWorld().getHighestBlockYAt(l.getBlockX(), l.getBlockZ()) <= l.getBlockY())
-                            chanceOfStrike += 50;
-                        for (Entity nearby : p.getNearbyEntities(5, 2, 5)) {
-                            if (nearby instanceof Player) {
-                                playersNearby += 1;
-                                chanceOfStrike += 20;
-                            }
-                        }
-
-                        // Spawn lightning based of player chance of it
-                        if (r.nextInt(100) < chanceOfStrike) {
-                            LightningStrike ls = map.getWorld().spawn(map.getWorld().getHighestBlockAt(l.getBlockX(), l.getBlockZ()).getLocation(), LightningStrike.class);
-                            Block b = ls.getLocation().getBlock();
-                            for (int x = b.getX() - 1; x <= b.getX() + 1; x++) {
-                                for (int y = b.getY() - 1; y <= b.getY() + 1; y++) {
-                                    for (int z = b.getZ() - 1; z <= b.getZ() + 1; z++) {
-                                        Block toBreak = map.getWorld().getBlockAt(x, y, z);
-                                        toBreak.breakNaturally(new ItemStack(Material.AIR));
-                                    }
-                                }
-                            }
-                        }
-
-                        // Add creeper charged spawn if there are more players nearby
-                        if (playersNearby > 0)
-                            if (!creeperChargedSpawns.containsKey(l))
-                                creeperChargedSpawns.put(l, playersNearby);
-
-                    }
-                }
-
-            if (creeperChargedSpawns.size() > 0) {
-                for (Map.Entry<Location, Integer> entry : creeperChargedSpawns.entrySet()) {
-                    if (r.nextInt(4) <= entry.getValue()) {
-                        Location l = entry.getKey();
-                        boolean spawned = false;
-                        while (!spawned) {
-                            Location temp = l.add(r.nextInt(7) - 3, 0, r.nextInt(7) - 3);
-                            if (temp.getBlock().getRelative(BlockFace.UP).getType() == Material.AIR) {
-                                Creeper c = map.getWorld().spawn(temp, Creeper.class);
-                                c.setPowered(true);
-                                spawned = true;
-                            }
-                        }
-                    }
-                }
-                creeperChargedSpawns.clear();
+                }, r.nextInt(3 * 20));
             }
 
+            // Spawn 1 more lightning strike per cycle each 10s
+            if (randomStrikesTimesRan.incrementAndGet() % 2 == 0) {
+                randomLightningStrikes.incrementAndGet();
+            }
 
-        }, startDelay, 20L);
+        }, startDelay, 5 * 20);
 
-        registerTasks(taskId);
+        // Spawn lightning strikes somewhere around the players. The dispersion varies based on some conditions
+        int lightningOnPlayerTask = Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> {
+
+            map.getPlayersInArena().forEach(p -> {
+                int y = p.getLocation().getBlockY();
+                int dispersion = 25;
+
+                // Decrease the dispersion based on the player Y level
+                int heightPenalityStep = Math.floorDiv(map.getMapSize().getY(), 10);
+                int heightPenality = Math.floorDiv(y - map.getMinY(), heightPenalityStep);
+                dispersion -= heightPenality;
+
+                // Decrease the dispersion if there's no blocks above the players head
+                if (y >= map.getWorld().getHighestBlockYAt(p.getLocation().getBlockX(), p.getLocation().getBlockZ())) {
+                    dispersion -= 5;
+                }
+
+                // Decrease the dispersion in 1 for each player nearby
+                var playersNearby = p.getNearbyEntities(3, 3, 3).stream().filter(e -> e instanceof Player).count();
+                dispersion -= (int) playersNearby;
+
+                // Spawn the lightning strike somewhere in a box centered around the player based on the dispersion
+                int finalDispersion = Math.max(dispersion, 0);
+                Bukkit.getScheduler().scheduleSyncDelayedTask(main, () ->
+                        spawnLightningStrikeAt(map.getWorld().getHighestBlockAt(
+                                p.getLocation().getBlockX() - finalDispersion + r.nextInt(2 * finalDispersion),
+                                p.getLocation().getBlockZ() - finalDispersion + r.nextInt(2 * finalDispersion)
+                        ).getLocation())
+                , r.nextInt(2 * 20));
+            });
+
+        }, startDelay, 3 * 20);
+
+        registerTasks(randomLightningTask, lightningOnPlayerTask);
 
     }
 }
