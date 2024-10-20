8f8fcbfd70630405939c750ef8b0bffe3a5c1567
tcklpl <negroniluan@gmail.com>
2024-10-20 20:11:16 -0300

    updated packets to 1.21.1
    Signed-off-by: tcklpl <negroniluan@gmail.com>

--- src/main/java/me/tcklpl/naturaldisaster/reflection/Packets.java
+++ src/main/java/me/tcklpl/naturaldisaster/reflection/Packets.java
@@ -1,13 +1,15 @@
 package me.tcklpl.naturaldisaster.reflection;
 
 import net.minecraft.network.protocol.game.*;
+import net.minecraft.server.level.ServerEntity;
 import org.bukkit.Chunk;
-import org.bukkit.craftbukkit.v1_20_R1.CraftChunk;
-import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_21_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_21_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import java.util.BitSet;
 import java.util.Collections;
+import java.util.Set;
 
 /**
  * Class with packets used in the plugin, used to call all packets using reflection and to keep normal classes cleaner
@@ -28,29 +30,17 @@ public class Packets {
             return new ClientboundRemoveEntitiesPacket(entityId);
         }
 
-        public static ClientboundAddPlayerPacket PlayOutNamedEntitySpawn(Player p) {
-            return new ClientboundAddPlayerPacket(((CraftPlayer)p).getHandle());
+        public static ClientboundAddEntityPacket PlayOutNamedEntitySpawn(Player p) {
+            var cp = (CraftPlayer) p;
+            var sp = cp.getHandle();
+            var serverEntity = new ServerEntity(sp.serverLevel(), sp, 0, false, packet -> {}, Set.of());
+
+            return new ClientboundAddEntityPacket(cp.getHandle(), serverEntity);
         }
 
         public static ClientboundLevelChunkWithLightPacket PlayOutMapChunk(Chunk chunkToUpdate) {
             var levelChunk = ((CraftChunk) chunkToUpdate).getCraftWorld().getHandle().getLevel().getChunk(chunkToUpdate.getX(), chunkToUpdate.getZ());
             return new ClientboundLevelChunkWithLightPacket(levelChunk, levelChunk.getLevel().getLightEngine(), new BitSet(), new BitSet());
-//            PacketContainer mapChunk = new PacketContainer(PacketType.Play.Server.MAP_CHUNK);
-//            mapChunk.get
-//            new ClientboundLevelChunkPacketData(chunkToUpdate.)
-//            try {
-//                Object nmsChunk = chunkToUpdate.getClass().getMethod("getHandle").invoke(chunkToUpdate);
-//                Constructor<?> mapChunk = Objects.requireNonNull(ReflectionUtils.getNMSClass("PacketPlayOutMapChunk"))
-//                        .getConstructor(
-//                                ReflectionUtils.getNMSClass("Chunk"),
-//                                int.class
-//                        );
-//                //return mapChunk.newInstance(nmsChunk, 65535, true);
-//                return mapChunk.newInstance(nmsChunk, 65535);
-//            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
-//                e.printStackTrace();
-//                return null;
-//            }
         }
 
     }
